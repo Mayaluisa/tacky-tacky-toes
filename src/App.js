279b86c1dc52233693425fc89e14fc3f44a8b9d8
Mayaluisa <mayaluisa.skynetwork@gmail.com>
2024-01-11 12:10:54 -0500

    Dependencies were downloaded, game was given a title.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TicTacToeFile from './components/TicTacToeFile';
+import TicTacToe from './components/TicTacToeFile';
+
+function App() {
+  return (
+    <TicTacToeFile/>
+  );
+}
+
+export default App;
---
+++ src/components/TicTacToeFile.jsx
@@ -0,0 +1,7 @@
+const tictacToeFx = () => {
+    return <div>
+        <h1>Tic Tac Toe</h1>
+    </div>
+}
+
+export default tictacToeFx
